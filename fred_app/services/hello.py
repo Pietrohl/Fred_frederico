0994b84d2e55297588a02aace99b1acfcc8b019e
Macario Moura <macario.moura@gmail.com>
2024-06-10 14:41:38 -0300

    changed hello_service to list_service

--- fred_app/services/hello.py
+++
@@ -1,4 +0,0 @@
-
-class HelloService:
-    def say_hello(self):
-        return "Hello, world!"
---
+++ fred_app/services/list_service.py
@@ -0,0 +1,5 @@
+class ListService():
+    def __init__(self, name: str = None):
+        if name is None:
+            raise ValueError("Name is required")
+        self.name = name
